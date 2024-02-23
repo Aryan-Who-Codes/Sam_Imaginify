e36fa68bfcc7a333fd8390ed1d19cb98734fa9f2
Sharma-Ar <aryanmay0305@gmail.com>
2024-02-23 16:17:11 +0530

    Update Dependencies and fix UI issues

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,18 +1,20 @@
+import Footer from '@/components/shared/Footer'
 import MobileNav from '@/components/shared/MobileNav'
 import Sidebar from '@/components/shared/Sidebar'
-import React from 'react'
+import { Toaster } from '@/components/ui/toaster'
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <main className='root'>
       <Sidebar />
       <MobileNav />
-
       <div className='root-container'>
         <div className='wrapper'>
           {children}
+          <Footer />
         </div>
       </div>
+      <Toaster />
     </main>
   )
 }
